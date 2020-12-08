52877939b6e1211c1f4e2bc27b7bc49ec0699b12
Mohamad <kattansoftware@gmail.com>
2020-12-07 21:47:41 -0800

    new update 8/12/2020

--- lib/pages/uploadImagePage.dart
+++ lib/pages/uploadImagePage.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:momants/pages/HomePage.dart';
 
 class UploadImagePage extends StatefulWidget {
   @override
@@ -17,6 +18,9 @@ class _UploadImagePageState extends State<UploadImagePage> {
   final formkey = GlobalKey<FormState>();
   // for on saved textformfiald
   String _myValue;
+  //this for url StorageFirebase
+  String url;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,9 +67,9 @@ class _UploadImagePageState extends State<UploadImagePage> {
                 height: 15.0,
               ),
               Padding(
-                padding:  EdgeInsets.all(8.0),
+                padding: EdgeInsets.all(8.0),
                 child: RaisedButton(
-                  onPressed: validateAndSave,
+                  onPressed: UploadStatusImage,
                   elevation: 10.0,
                   textColor: Colors.white,
                   color: Colors.amber,
@@ -96,5 +100,46 @@ class _UploadImagePageState extends State<UploadImagePage> {
     } else {
       return false;
     }
+  }
+
+//this method for upload Image to storage
+  void UploadStatusImage() async {
+    if (validateAndSave()) {
+      final StorageReference postImageRef =
+          FirebaseStorage.instance.ref().child('postImage');
+
+      // this time key it will be random key = post ID
+      var timeKey = DateTime.now();
+      final StorageUploadTask task =
+          postImageRef.child(timeKey.toString() + '.jpg').putFile(sampleImage);
+      var imageUrl = await (await task.onComplete).ref.getDownloadURL();
+      url = imageUrl.toString();
+      print('imageUr=' + url);
+      goToHomePage();
+      saveToDatbase(url);
+    }
+  }
+
+//this method for save data in database
+  void saveToDatbase(String url) {
+    // this time key it will be random key = post ID
+    var dbtimeKey = DateTime.now();
+    var formatDate = DateFormat('MMM d,yyyy');
+    var formatTime = DateFormat('EEEE, hh:mm');
+    //this for put time+date with random key for every id post
+    String date = formatDate.format(dbtimeKey);
+    String time = formatTime.format(dbtimeKey);
+
+    DatabaseReference ref = FirebaseDatabase.instance.reference();
+
+    var data = {'image': url, 'Descr': _myValue, 'time': time, 'date': date};
+    ref.child('posts').push().set(data);
+  }
+
+// this method for puch to home page
+  void goToHomePage() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return HomePage();
+    }));
   }
 }
